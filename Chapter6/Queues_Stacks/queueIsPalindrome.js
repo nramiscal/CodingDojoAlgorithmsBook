2d3cab0ae570dfeefe27307c4eb197fd0e41fa0f
nramiscal <nramiscal@gmail.com>
2018-03-21 13:32:45 -0400

    isPalindrome

--- Chapter6/Queues_Stacks/queueIsPalindrome.js
+++ Chapter6/Queues_Stacks/queueIsPalindrome.js
@@ -81,7 +81,8 @@ function SLQueue() {
 }
 
 
-// Given a Queue , return true if its values are a palindrome (if they are same in // reverse order), else return false . Restore Queue to original state before
+// Given a Queue , return true if its values are a palindrome (if they are same in
+// reverse order), else return false . Restore Queue to original state before
 // exiting. For storage, use one additional Stack .
 function isPalindrome(queue){
     var stack = new SLStack();
