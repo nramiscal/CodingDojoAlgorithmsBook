487f7f36ef9f90c1323b85f12d6ea5b5017c5713
nramiscal <nramiscal@gmail.com>
2018-04-03 12:03:37 -0400

    reverseSLL

---
+++ Chapter8/reverseSLL.js
@@ -0,0 +1,53 @@
+function Node(value){
+    this.val = value;
+    this.next = null;
+}
+
+function SLL(){
+    this.head = null;
+
+    this.add = function(value) {
+        var newNode = new Node(value);
+        if(!this.head) {this.head = newNode;}
+        else {
+            var runner = this.head;
+            while (runner.next){
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+        return this;
+    }
+
+    this.reverse = function(){
+        var prev = null;
+        var current = this.head;
+        var follow = this.head.next;
+
+        while(follow){
+            current.next = prev;
+            prev = current;
+            current = follow;
+            follow = follow.next;
+        }
+        current.next = prev;
+        this.head.next = null;
+        this.head = current;
+        return this;
+    }
+
+    this.printAsArray = function(){
+        var runner = this.head;
+        var arr = [];
+        while (runner){
+            arr.push(runner.val);
+            runner = runner.next;
+        }
+        console.log(arr);
+        return this;
+    }
+}
+
+var sList = new SLL();
+sList.add(1).add(2).add(3).add(4).add(5).printAsArray();
+sList.reverse().printAsArray();
