f798c0bf7ac40376c9a321b8ccd3a20a0e7651a5
nramiscal <nramiscal@gmail.com>
2018-04-04 14:49:43 -0400

    removed console.logs

--- Chapter7/matrixSearch.js
+++ Chapter7/matrixSearch.js
@@ -16,14 +16,10 @@
 //             [43, 32]]
 //
 // return true.
-//
-// Second: Return location of first match found ( [-1,-1] if no match).
-// In example above, return [2,1] .
 
 
 function matrixSearch(matrix, sub){
     let corner = sub[0][0];
-    // console.log(corner); // 67
     let flag = false;
 
     for (let i = 0; i < matrix.length; i++){
@@ -31,27 +27,17 @@ function matrixSearch(matrix, sub){
             if (matrix[i][j] == corner){
                 flag = true;
                 // as soon as we match corner, start checking adjacent values
-
-                // console.log(matrix[i][j]); // 67
-                // console.log(i); // 2
-                // console.log(j); // 1
-                // console.log(matrix[i][j+1]); // 78
-                // console.log(matrix[i+1][j]); // 43
-                // console.log(matrix[i+1][j+1]); // 32
-
                 // we have a match at matrix[i][j] with sub[k=0][l=0]
                 for (let k = 0; k < sub.length; k++){
                     for (let l = 0; l < sub[k].length; l++){
-
-                        // console.log(sub[k][l]); // 67 78 43 32
-                        // console.log(matrix[i+k][j+l]); // 67 78 43 32
-
-                        if (i+k < matrix.length &&  j+l < matrix[i].length){ // only check if valid index in matrix
+                        // only check if valid index in matrix
+                        if (i+k < matrix.length &&  j+l < matrix[i].length){
                             if (sub[k][l] !== matrix[i+k][j+l]){
                                 flag = false;
                                 break;
                             }
                         }
+                        // if sub would extend past boundaries of matrix, reset flag to false
                         else {
                             flag = false;
                         }
