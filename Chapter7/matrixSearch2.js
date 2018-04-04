d313144bb9a3556d51bad3da8c7f134153c0671f
nramiscal <nramiscal@gmail.com>
2018-04-04 15:16:02 -0400

    returns location of first match found

--- Chapter7/matrixSearch2.js
+++ Chapter7/matrixSearch2.js
@@ -1,27 +1,18 @@
 // Matrix Search
 //
-// Mike digs image recognition and wants to create a JavaScript Imaging
-// Library, just like PIL for Python. He is given 2 different two-dimensional
-// arrays, containing integers between 0 and 65535. Each two-dimensional
-// array represents a gray-scale image, where each integer value is a pixel.
-// The second image might be found somewhere within the larger one. Return
-// whether it is.
-//
 // Given array: [[12, 34, 45, 56],
 //               [98, 87, 76, 65],
 //               [56, 67, 78, 89],
 //               [54, 43, 32, 21]]
-//
 // and array: [[67, 78],
 //             [43, 32]]
-//
 // return true.
 //
-// Second: Return location of first match found ( [-1,-1] if no match). In example above, return [2,1] .
+// Second: Return location of first match found ( [-1,-1] if no match). In example above, return [2,1].
 
 
 
-function matrixSearch(matrix, sub){
+function matrixSearch2(matrix, sub){
     let corner = sub[0][0];
     let flag = false;
 
@@ -50,13 +41,13 @@ function matrixSearch(matrix, sub){
                 }
                 // as soon as we find a perfect match, return true
                 if (flag){
-                    return true;
+                    return [i,j];
                 }
             }
         }
     }
-    return flag;
+    return [-1,-1];
 }
 
 
-console.log(matrixSearch([[12, 34, 45, 56],[98, 87, 76, 65],[56, 67, 78, 89],[54, 43, 32, 21]], [[67, 78],[43, 32]]));
+console.log(matrixSearch2([[12, 34, 45, 56],[98, 87, 76, 65],[56, 67, 78, 89],[54, 43, 32, 21]], [[65, 78],[43, 32]]));
